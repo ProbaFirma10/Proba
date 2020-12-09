c207d05ad04c53ab3a9fd733d9b6a94dbaf4e626
Dusan Trkulja <dtrkulja5@gmail.com>
2020-12-09 11:13:49 +0100

    #proba

--- PatientWebApplication/PatientWebApplication/Startup.cs
+++ PatientWebApplication/PatientWebApplication/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PatientWebApplication.Validators;
+using System;
 
 namespace PatientWebApplication
 {
@@ -40,13 +41,25 @@ namespace PatientWebApplication
             
 
             services.AddDbContext<MyDbContext>(options =>
-            options.UseMySql(ConfigurationExtensions.GetConnectionString(Configuration, "MyDbContextConnectionString")).UseLazyLoadingProxies());
+            options.UseMySql(CreateConnectionStringFromEnvironment()).UseLazyLoadingProxies());
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
+        }
+
+        private string CreateConnectionStringFromEnvironment()
+        {
+            string server = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
+            string port = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? "3306";
+            string database = Environment.GetEnvironmentVariable("DATABASE_SCHEMA") ?? "MYSQLHealtcareDB";
+            string user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "root";
+            string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "root";
+
+
+            return $"server={server};port={port};database={database};user={user};password={password};";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

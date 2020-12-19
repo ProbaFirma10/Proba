2732af6d25eb11611c922e0bb490c9225be29179
Dusan Trkulja <dtrkulja5@gmail.com>
2020-12-19 13:33:31 +0100

    #izmjena startup-a

--- PatientWebApplication/PatientWebApplication/Startup.cs
+++ PatientWebApplication/PatientWebApplication/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PatientWebApplication.Validators;
-
+using System;
 namespace PatientWebApplication
 {
     public class Startup
@@ -37,16 +37,16 @@ namespace PatientWebApplication
                options.RegisterValidatorsFromAssemblyContaining<Startup>();
            });
 
-            /*if (CurrentEnvironment.IsEnvironment("Testing"))
+            if (CurrentEnvironment.IsEnvironment("Testing"))
             {
                 services.AddDbContext<MyDbContext>(options =>
                     options.UseInMemoryDatabase("TestingDB").UseLazyLoadingProxies());
             }
             else
-            {*/
+            {
                 services.AddDbContext<MyDbContext>(options =>
-                options.UseMySql(ConfigurationExtensions.GetConnectionString(Configuration, "MyDbContextConnectionString")).UseLazyLoadingProxies());
-            //}
+                options.UseMySql(CreateConnectionStringFromEnvironment()).UseLazyLoadingProxies());
+            }
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -54,6 +54,20 @@ namespace PatientWebApplication
                 configuration.RootPath = "ClientApp/build";
             });
         }
+
+        private string CreateConnectionStringFromEnvironment()
+        {
+            string server = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
+            string port = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? "3306";
+            string database = Environment.GetEnvironmentVariable("DATABASE_SCHEMA") ?? "MYSQLHealtcareDB";
+            string user = Environment.GetEnvironmentVariable("DATABASE_USERNAME") ?? "root";
+            string password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "root";
+
+
+            return $"server={server};port={port};database={database};user={user};password={password};";
+        }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
